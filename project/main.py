de9029a1e84a637f2757059400c9b71f2d253b26
AsteXOriginal <117456510+AsteXOriginal@users.noreply.github.com>
2023-05-23 18:59:03 +0300

    Update main.py

--- project/main.py
+++ project/main.py
@@ -1,14 +1,25 @@
-password = 1432
+try:
+    userDb = {
+        'alex@gmail': 'alex123',
+        '1': '1',
+        'oleh@icloud': 'oleh89',
+        '2': '2'
+    }
 
-i = 0
-while i < 3:
-    checkpassword = int(input("password -> "))
-    if checkpassword == password:
-        print('registration is successful')
-        break
-    if i <= 1: 
-        print('registration is failed, please try again')
-   
-    else:
-        print('unauthorized access attempt')
-    i += 1
+    i = 0
+    while i < 3:
+
+        checklogin = str(input("login -> "))
+        checkpassword = str(input("password -> "))
+
+        if checklogin in userDb and userDb[checklogin] == checkpassword:
+            print('registration is successful')
+            break
+        if i <= 1:
+            print('registration is failed, please try again')
+        else:
+            print('unauthorized access attempt')
+        i += 1
+
+except ValueError:
+    print('Please, input your correct data')
